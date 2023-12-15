44d6ab570abac1c363471625d46b55eca24ba933
AnsarBM <82270042+AnsarBM@users.noreply.github.com>
2023-12-14 23:21:49 -0500

    Add files via upload

---
+++ write-concise-object-literal-declarations-using-object-property-shorthand.js
@@ -0,0 +1,11 @@
+const createPerson = (name, age, gender) => ({
+  // Only change code below this line
+  /*return {
+    name: name,
+    age: age,
+    gender: gender
+  };*/
+   name, age, gender
+
+  // Only change code above this line
+});
